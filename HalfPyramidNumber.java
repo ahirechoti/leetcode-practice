5fe57c0a10974c703211d19c873e1bdf1c9a19e2
Abhishek H V <101446774+ahirechoti@users.noreply.github.com>
2022-10-11 22:05:24 +0530

    update

--- HalfPyramidNumber.java
+++ HalfPyramidNumber.java
@@ -11,8 +11,8 @@ public class HalfPyramidNumber {
 		int n = sc.nextInt();
 		
 		for(int i = 1; i<=n;i++) {
-			for(int j=0;j<i;j++) {
-				System.out.print((j+1)+" ");
+			for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
 			}
 			System.out.println("");
 		}
